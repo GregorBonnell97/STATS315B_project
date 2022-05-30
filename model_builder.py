6e239c5fd92a7e888e2ab58c6e353249b60816a1
Adrien Lemercier <lmercier@stanford.edu>
2022-05-30 10:52:22 -0700

    parallel computing works

--- model_builder.py
+++ model_builder.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from cwgradient import CWGradientBoosting
 from multiprocessing import Pool
+import os
 
 if len(sys.argv)!=3:
     print("Please run scripts with following arguments:\n-zone\n-M")
@@ -18,7 +19,8 @@ def processing(spacetime):
     dates=data.date.values
     hours=data.hour.values
     zones=data.zone_id.values
-    data.drop(columns=["Unnamed: 0","zone_id","station_id","hour","date","load","temp_max_24_48.1","temp_avg_24.1"],inplace=True)
+    data.drop(columns=["Unnamed: 0","zone_id","station_id","hour","date","load"],inplace=True)
+    # "temp_max_24_48.1","temp_avg_24.1" are removed
     data["One"]=np.ones(Y.size)
     X=data.values
     Y_train,Y_val,Y_test=Y[:-140],Y[-140:-70],Y[-70:]
@@ -28,7 +30,9 @@ def processing(spacetime):
     val=pd.DataFrame(data={"date":dates[-140:-70],"hour":hours[-140:-70],"pred":cwg.val_estimator,"values":Y_val})
     test=pd.DataFrame(data={"date":dates[-70:], "hour":hours[-70:],"pred":cwg.test_estimator, "values":Y_test})
     val_error=np.sum((cwg.val_estimator-Y_val)**2)/1e06
-    f=open("val_errors/{}_{}_{}.txt".format(zone,station,hour),"w")
+
+    f=open("val_errors/{}_{}_{}.txt".format(zone,station,hour), "w+") 
+    # creates file if it doesn t already exist, otherwise overwrite 
     f.write(str(val_error))
     f.close()
     val.to_csv("val_pred/pred_{}_{}_{}.csv".format(zone,station,hour))
@@ -38,12 +42,17 @@ def processing(spacetime):
 if __name__ == "__main__":
 
     l = []
-    for station in range(1,12):
-        for hour in range(1,24):
+    # don t ask too much to your RAM
+    for station in range(1,12):  # 1-> 12
+        for hour in range(6,12):  # 1 -> 24
             l.append([station, hour])
-            # processing(station, hour)
 
-    print("hello !!!")
+    if not os.path.exists("val_errors"):
+        os.mkdir("val_errors")
+    if not os.path.exists("val_pred"):
+        os.mkdir("val_pred")
+    if not os.path.exists("test_pred"):
+        os.mkdir("test_pred")
 
     with Pool(len(l)+1) as p:
         print(p.map(processing, l))
